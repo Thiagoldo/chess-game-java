7e6c360c0501ff33ae17017cce04cf88e4a8b2ba
Thiago Lima <thiagolimadeoliveira18@gmail.com>
2023-07-29 20:39:56 -0300

    Start to implement Board and Piece

--- src/application/App.java
+++ src/application/App.java
@@ -1,6 +1,9 @@
 package application;
+
+import board.Board;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Board board = new Board(8, 8);
     }
 }
---
+++ src/board/Board.java
@@ -0,0 +1,27 @@
+package board;
+
+public class Board {
+    private Integer rows;
+    private Integer columns;
+
+    private Piece[][] pieces;
+
+    public Board(Integer rows, Integer columns) {
+        this.rows = rows;
+        this.columns = columns;
+        pieces = new Piece[rows][columns];
+    }
+
+    public Integer getRows() {
+        return rows;
+    }
+
+    public Integer getColumns() {
+        return columns;
+    }
+
+    public Piece[][] getPieces() {
+        return pieces;
+    }
+
+}
